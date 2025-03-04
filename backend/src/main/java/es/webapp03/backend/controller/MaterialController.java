ddb36a8cd757d13007820e4fc354bb9c5291134b
Markos9 <pascualmarcos96@gmail.com>
2025-03-04 09:53:05 +0100

    added csrf token to html pages

--- backend/src/main/java/es/webapp03/backend/controller/MaterialController.java
+++ backend/src/main/java/es/webapp03/backend/controller/MaterialController.java
@@ -1,27 +1,22 @@
 package es.webapp03.backend.controller;
 
-import java.io.File;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import es.webapp03.backend.model.Course;
@@ -30,7 +25,7 @@ import es.webapp03.backend.repository.CourseRepository;
 import es.webapp03.backend.repository.MaterialRepository;
 import jakarta.servlet.http.HttpServletRequest;
 
-@RestController
+@Controller
 public class MaterialController {
     @Autowired
     private CourseRepository courseRepository;
