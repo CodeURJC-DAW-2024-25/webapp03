cd4a261d843449eb8864382137ae2339727e0f58
sergio2000-hub <sergetealdea@gmail.com>
2025-06-18 12:52:12 +0200

    UserList Done but delete and loadMore

--- frontend/libace/src/app/components/user-list/user-list.component.ts
+++ frontend/libace/src/app/components/user-list/user-list.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { UserBasicDTO } from '../../dtos/userBasic.dto';
+import { LoginService } from '../../services/login.service';
 
 @Component({
-  selector: 'app-user-list',
+  selector: 'app-admin-users',
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  deleteUser(arg0: number | undefined) {
+    throw new Error('Method not implemented.');
+  }
+  users: UserBasicDTO[] = []; // Usamos UserBasicDTO aquí
+  currentPage = 0;
+  loading = false;
+  allLoaded = false;
 
+  constructor(
+    private userService: UserService,
+    private loginService: LoginService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    if (this.loading || this.allLoaded) return;
+
+    this.loading = true;
+    this.userService.getAllBasicUsers(this.currentPage, 3).subscribe({
+      next: (newUsers) => {
+        if (newUsers.length === 0) {
+          this.allLoaded = true;
+        } else {
+          this.users.push(...newUsers);
+          this.currentPage++;
+        }
+      },
+      error: (err) => {
+        console.error('Error loading users:', err);
+        this.loading = false;
+      },
+    });
+  }
+
+  get isAdmin(): boolean {
+    return this.loginService.isAdmin();
+  }
 }
--- frontend/libace/src/app/services/user.service.ts
+++ frontend/libace/src/app/services/user.service.ts
@@ -1,23 +1,40 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { UserDTO } from "../dtos/user.dto";
-import { Observable } from "rxjs";
-
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { UserDTO } from '../dtos/user.dto';
+import { UserBasicDTO } from '../dtos/userBasic.dto';
+import { catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class UserService {
   private apiUrl = '/api/users';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getCurrentUser(): Observable<UserDTO> {
-    return this.http.get<UserDTO>(`${this.apiUrl}/me`, { withCredentials: true }); //Really needed for cookies? TODO
+    return this.http.get<UserDTO>(`${this.apiUrl}/me`, {
+      withCredentials: true,
+    }); //Really needed for cookies? TODO
+  }
+  getAllBasicUsers(page: number, size: number): Observable<UserBasicDTO[]> {
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString());
+    return this.http
+      .get<{ content: UserBasicDTO[] }>(`${this.apiUrl}/`, {
+        params,
+        withCredentials: true,
+      })
+      .pipe(
+        map((response) => response.content),
+        catchError((error) => this.handleError(error))
+      );
   }
 
+  private handleError(error: any) {
+    console.log('error:');
+    console.error(error);
+    return throwError('Server Error (' + error.status + '): ' + error.text());
+  }
 }
-
-
-
