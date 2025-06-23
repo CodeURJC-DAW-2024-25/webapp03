99588869e51d4912794dd549e3e95c1334a0f09a
DavidCCGithubURJC <d.cereceda.2021@alumnos.urjc.es>
2025-06-23 19:44:15 +0200

    Fixed user-list header and deleted unused commponents

--- frontend/libace/src/app/app.module.ts
+++ frontend/libace/src/app/app.module.ts
@@ -14,18 +14,9 @@ import { RegisterComponent } from './components/register/register.component';
 import { EditCourseComponent } from './components/edit-course/edit-course.component';
 import { ModifyProfileComponent } from './components/modify-profile/modify-profile.component';
 import { NewCourseComponent } from './components/new-course/new-course.component';
-import { AdminUsersComponent } from './components/admin-users/admin-users.component';
-import { CommentListComponent } from './components/comment-list/comment-list.component';
-import { CourseListComponent } from './components/course-list/course-list.component';
-import { MaterialListComponent } from './components/material-list/material-list.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { LoginErrorComponent } from './components/login-error/login-error.component';
-import { RegisterErrorComponent } from './components/register-error/register-error.component';
 import { ErrorComponent } from './components/error/error.component';
-import { Error403Component } from './components/error403/error403.component';
-import { Error404Component } from './components/error404/error404.component';
-import { Error500Component } from './components/error500/error500.component';
 import { ChartComponent } from './components/chart/chart.component';
 
 @NgModule({
@@ -39,18 +30,9 @@ import { ChartComponent } from './components/chart/chart.component';
     EditCourseComponent,
     ModifyProfileComponent,
     NewCourseComponent,
-    AdminUsersComponent,
-    CommentListComponent,
-    CourseListComponent,
-    MaterialListComponent,
     UserListComponent,
     HomePageComponent,
-    LoginErrorComponent,
-    RegisterErrorComponent,
     ErrorComponent,
-    Error403Component,
-    Error404Component,
-    Error500Component,
     ChartComponent
   ],
   imports: [
--- frontend/libace/src/app/components/user-list/user-list.component.ts
+++ frontend/libace/src/app/components/user-list/user-list.component.ts
@@ -5,7 +5,7 @@ import { LoginService } from '../../services/login.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-admin-users',
+  selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
 })
