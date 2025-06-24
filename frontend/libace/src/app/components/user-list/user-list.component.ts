93197e09279ab0a057724baf860814ec9a519cd8
DavidCCGithubURJC <d.cereceda.2021@alumnos.urjc.es>
2025-06-24 14:39:43 +0200

    Removed useless comments

--- frontend/libace/src/app/components/user-list/user-list.component.ts
+++ frontend/libace/src/app/components/user-list/user-list.component.ts
@@ -64,7 +64,7 @@ export class UserListComponent implements OnInit {
   }
 
   private checkAccess(): void {
-    this.loginService.reqIsLogged(); // Update el estado de autenticación
+    this.loginService.reqIsLogged();
 
     // Verify if the user is logged in and is an admin
     setTimeout(() => {
